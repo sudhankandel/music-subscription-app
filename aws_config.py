d6a0ea3d50ffb9dde535cf75a04551085ab23d57
sudhankandel <sudhankandel03@gmail.com>
2026-05-02 16:18:14 +1000

    Implement API based app structure

---
+++ aws_config.py
@@ -0,0 +1,12 @@
+import boto3
+
+REGION_NAME = "us-east-1"
+
+dynamodb = boto3.resource("dynamodb", region_name=REGION_NAME)
+s3_client = boto3.client("s3", region_name=REGION_NAME)
+
+login_table = dynamodb.Table("login")
+music_table = dynamodb.Table("music")
+subscription_table = dynamodb.Table("subscriptions")
+
+S3_BUCKET_NAME = "your-bucket-name"
